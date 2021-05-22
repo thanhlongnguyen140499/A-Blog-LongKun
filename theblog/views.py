0800e5f7b7e05871f97694cea976990573e169d2
thanhlongnguyen140499 <thanhlongnguyen140499@gmail.com>
2021-05-22 22:55:57 +0700

    Unlike feature

--- theblog/views.py
+++ theblog/views.py
@@ -13,7 +13,13 @@ from django.http import HttpResponseRedirect
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id = request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
     return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
 
@@ -38,8 +44,15 @@ class ArticleDetailView(DetailView):
         categories = Category.objects.all()
         context = super(ArticleDetailView, self).get_context_data(**kwargs)
         context['categories'] = categories
+
         stuff = get_object_or_404(Post, id = self.kwargs['pk'])
         total_likes = stuff.total_likes()
+
+        liked = False
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        context['liked'] = liked
+
         context['total_likes'] = total_likes
         return context
 
