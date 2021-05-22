5b4cfda1734bfb3ca668bfbf5c5902cb03e9bfab
thanhlongnguyen140499 <thanhlongnguyen140499@gmail.com>
2021-05-23 00:20:46 +0700

    Modify UserCreationForm with create SignUpForm

--- members/views.py
+++ members/views.py
@@ -3,8 +3,9 @@ from django.db import models
 from django.shortcuts import render
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 # Create your views here.
 class UserRegisterView(CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
