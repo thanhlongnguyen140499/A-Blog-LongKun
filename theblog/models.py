95d12588199abad4372a276db77d7e9e5351ce61
thanhlongnguyen140499 <thanhlongnguyen140499@gmail.com>
2021-05-25 00:28:03 +0700

    Add Profile Pic and Social Media Links in Article Detail View

--- theblog/models.py
+++ theblog/models.py
@@ -22,6 +22,12 @@ class Category(models.Model):
 class Profile(models.Model):
     user = OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(default='bio of user')
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="profile/")
+    website_url = models.CharField(max_length=255, null=True, blank=True)
+    fb_url = models.CharField(max_length=255, null=True, blank=True)
+    twitter_url = models.CharField(max_length=255, null=True, blank=True)
+    instagram_url = models.CharField(max_length=255, null=True, blank=True)
+    pinterest_url = models.CharField(max_length=255, null=True, blank=True)
     
     def __str__(self):
         return str(self.user)
